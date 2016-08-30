30da7468b7f4eb6816587fb77b4dc3782721ac06
Sandra Tsai <sandraytsai@gmail.com>
2016-08-30 17:23:13 +0000

    append to csv edited

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ var request = require('request');
 request('http://substack.net/images/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(body);
+    fs.appendFile('images.csv', 'File Permission, Absolute URL, File Type \n', function(err) {
+      if (error) {
+        return console.error(err);
+      };
+    });
     $('tr').each(function(i, element){
       var permission = $(this).children().first();
       var url = $(this).find('a').attr('href');
       if (url.charAt(url.length-4) == ".") {
         var filetype = url.substring(url.length - 4);
       };
-      fs.appendFile('images.csv', permission.text() + ',', function(err) {
-        if (error) {
-          return console.error(err);
-        };
-      });
-      fs.appendFile('images.csv', url + ',', function(err) {
-        if (error) {
-          return console.error(err);
-        };
-      });
-      fs.appendFile('images.csv', filetype + '\n', function(err) {
+      fs.appendFile('images.csv', permission.text() + ',http://substack.net' + url + ',' + filetype + '\n', function(err) {
         if (error) {
           return console.error(err);
         };
